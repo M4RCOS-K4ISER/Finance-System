21e60125994fed56c8e5d827fc02cc72b2fc97c3
Marcos Kaiser <N4TSUNAGI@OUTLOOK.COM>
2025-09-14 21:46:03 -0300

    Add files via upload

---
+++ Finance System/src/main/java/com/example/demo/models/Banco.java
@@ -0,0 +1,8 @@
+package com.example.demo.models;
+
+public class Banco extends BaseModel {
+    private String nome;
+
+    public String getNome() {return nome;}
+    public void setNome(String nome) {this.nome = nome;}
+}
---
+++ Finance System/src/main/java/com/example/demo/models/BaseModel.java
@@ -0,0 +1,17 @@
+package com.example.demo.models;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+import java.io.Serializable;
+
+public class BaseModel implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {return id;}
+    public void setId(Long id) {this.id = id;}
+}
